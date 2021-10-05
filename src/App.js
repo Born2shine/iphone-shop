9f802f0590123a072b65fd69d489959cd77d3d1e
SpiderTech <odohfriday33@yahoo.com>
2021-10-05 15:20:58 +0100

    Added contextApi to prevent props drilling

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Home } from "./components/Home"
+import "./assets/css/main.css"
+
+function App() {
+  return (
+    <div className="App">
+        <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/provider/context.js
@@ -0,0 +1,18 @@
+import React, {useState, useContext } from "react"
+
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+
+    return (
+        <AppContext.Provider value={{
+            
+        }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => useContext(AppContext)
+
+export {AppContext, AppProvider}
