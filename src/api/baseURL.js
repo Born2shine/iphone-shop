1da829f8f6bf60c874c035046555268d99abf662
SpiderTech <odohfriday33@yahoo.com>
2021-10-06 14:51:11 +0100

    added CI build in package.json for netlify

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,8 +1,2 @@
 
-
-// const api = axios.create({
-//     baseURL: `https://ezeapi-prod-copy.herokuapp.com/api/v1`
-// })
 export const baseURL = `https://ezeapi-prod-copy.herokuapp.com/api/v1`
-
-// export default api
